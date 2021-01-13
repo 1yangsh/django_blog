62c42addc1baa83622d6f5300ac41af261b65a43
1yangsh <ysh410@gmail.com>
2021-01-13 17:20:30 +0900

    template update

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+
 from .models import Post
 
-def post_list_response(request):
-    name = "Django"
-    response = HttpResponse(content_type="text/html")
-    response.write(f'<h2>Hello {name}!!</h2>')
-    response.write(f'<p>HTTP METHOD : {request.method}</p>')
-    response.write(f'<p>HTTP ContentType : {request.content_type}</p>')
-    # return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
-    return response
+# Post 상세정보
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Post 목록
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_list_response(request):
+    name = 'Django'
+    response = HttpResponse(f'<h2>Hello {name}!!</h2>', content_type="text/html")
+    response.write(f'<h2>Hello {name}!!</h2>')
+    response.write(f'<p>HTTP Method : {request.method}</p>')
+    response.write(f'<p>HTTP ContentType : {request.content_type}</p>')
+    #return HttpResponse(f'''<h2>Hello {name}!!</h2><p>HTTP METHOD : {request.method}</p>''')
+    return response
+
