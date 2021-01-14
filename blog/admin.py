92b977bde6025a9504e0f30f625896b1213b1494
1yangsh <ysh410@gmail.com>
2021-01-14 16:29:55 +0900

    comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # admin page 출력되는 내용을 customize
 class PostAdmin(admin.ModelAdmin):
@@ -13,3 +13,4 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
